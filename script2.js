b69ed9fc42892965bf779f87f1171f1120dc3a99
Dauren <biboldauren@gmail.com>
2019-07-24 23:29:00 +0600

    first com

--- script2.js
+++ script2.js
@@ -1,83 +1,30 @@
 'use strict'
 
-// function showFirstMessage(text) {
-//     alert(text);
-//     console.log(num);
-// }
+// types of data
+var number = 4;
+var string = 'fff';
+var sym = Symbol();
+var bool = true;
+null;
+undefined;
+var obj = {};
 
-// let num = 30;
-// showFirstMessage("Hello world");
-
-
-
-function retVar() {
-    let a = 5;
-    return a;
+var person = {
+    name: 'John',
+    age: 25,
+    isMarried: true
 };
 
-let b = retVar();
-console.log(b);
+console.log(person.age);
 
+let arr = ['fdfdf', 5, true];
 
-let calc = (a, b) => a+b;
+console.log(arr[0]);
 
-console.log(calc(2, 2));
+// общение с пользователем
+// alert('hello world');
 
-console.log(calc(2, 5));
+// confirm('Are you here?');
 
-let str = "Hello";
-// console.log(str.length);
-
-console.log(str.toUpperCase());
-
-console.log(str.toLowerCase());
-
-let n = "12.5px";
-console.log(Math.round(n));
-console.log(parseFloat(n));
-
-
-
-// if (num == 49) {
-//     console.log("Неверно")
-// } else if (num > 100) {
-//     console.log("Много")
-// } else {
-//     console.log("Верно")
-// };
-
-// switch (num) {
-//     case num > 100:
-//         console.log("Много");
-//         break;
-//     case num < 10:
-//         console.log("Мало");
-//         break;
-//     case 50:
-//         console.log("Верно");
-//         break;
-//     default:
-//         console.log("blabla");
-//         break;
-// };
-
-
-// let num = 50;
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++
-// } 
-// while (num < 60);
-
-
-// for (let i = 1; i < 10; i++) {
-//     if (i == 4) {
-//         continue
-//     }
-//     console.log(i)
-// }
+let answer = prompt("Вам есть 18?", "");
+console.log(answer);
